15d9331169ea24a5a31e16a3356e49f46d3df35f
Jordan Harband <ljharb@gmail.com>
2015-06-24 21:44:15 -0700

    Update `eslint`

--- test.js
+++ test.js
@@ -1,25 +1,25 @@
 'use strict';
 
 var test = require('tape');
-var isNaN = require('./');
+var numberIsNaN = require('./');
 
 test('not NaN', function (t) {
-	t.notOk(isNaN(), 'undefined is not NaN');
-	t.notOk(isNaN(null), 'null is not NaN');
-	t.notOk(isNaN(false), 'false is not NaN');
-	t.notOk(isNaN(true), 'true is not NaN');
-	t.notOk(isNaN(0), 'positive zero is not NaN');
-	t.notOk(isNaN(Infinity), 'Infinity is not NaN');
-	t.notOk(isNaN(-Infinity), '-Infinity is not NaN');
-	t.notOk(isNaN('foo'), 'string is not NaN');
-	t.notOk(isNaN([]), 'array is not NaN');
-	t.notOk(isNaN({}), 'object is not NaN');
-	t.notOk(isNaN(function () {}), 'function is not NaN');
-	t.notOk(isNaN('NaN'), 'string NaN is not NaN');
+	t.notOk(numberIsNaN(), 'undefined is not NaN');
+	t.notOk(numberIsNaN(null), 'null is not NaN');
+	t.notOk(numberIsNaN(false), 'false is not NaN');
+	t.notOk(numberIsNaN(true), 'true is not NaN');
+	t.notOk(numberIsNaN(0), 'positive zero is not NaN');
+	t.notOk(numberIsNaN(Infinity), 'Infinity is not NaN');
+	t.notOk(numberIsNaN(-Infinity), '-Infinity is not NaN');
+	t.notOk(numberIsNaN('foo'), 'string is not NaN');
+	t.notOk(numberIsNaN([]), 'array is not NaN');
+	t.notOk(numberIsNaN({}), 'object is not NaN');
+	t.notOk(numberIsNaN(function () {}), 'function is not NaN');
+	t.notOk(numberIsNaN('NaN'), 'string NaN is not NaN');
 	t.test('valueOf', function (st) {
 		var obj = { valueOf: function () { return NaN; } };
-		st.ok(isNaN(Number(obj)), 'object with valueOf of NaN, converted to Number, is NaN');
-		st.notOk(isNaN(obj), 'object with valueOf of NaN is not NaN');
+		st.ok(numberIsNaN(Number(obj)), 'object with valueOf of NaN, converted to Number, is NaN');
+		st.notOk(numberIsNaN(obj), 'object with valueOf of NaN is not NaN');
 		st.end();
 	});
 
@@ -27,7 +27,7 @@ test('not NaN', function (t) {
 });
 
 test('NaN literal', function (t) {
-	t.ok(isNaN(NaN), 'NaN is NaN');
+	t.ok(numberIsNaN(NaN), 'NaN is NaN');
 	t.end();
 });
 
