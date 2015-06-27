66dea7a5762f8d51965b6bdc663e5de85cdc38d3
Jordan Harband <ljharb@gmail.com>
2015-06-27 00:37:36 -0700

    Update `eslint`

--- test/tests.js
+++ test/tests.js
@@ -2,18 +2,23 @@
 
 module.exports = function (numberIsNaN, t) {
 	t.test('not NaN', function (st) {
-		st.notOk(numberIsNaN(), 'undefined is not NaN');
-		st.notOk(numberIsNaN(null), 'null is not NaN');
-		st.notOk(numberIsNaN(false), 'false is not NaN');
-		st.notOk(numberIsNaN(true), 'true is not NaN');
-		st.notOk(numberIsNaN(0), 'positive zero is not NaN');
-		st.notOk(numberIsNaN(Infinity), 'Infinity is not NaN');
-		st.notOk(numberIsNaN(-Infinity), '-Infinity is not NaN');
-		st.notOk(numberIsNaN('foo'), 'string is not NaN');
+		st.test('primitives', function (sst) {
+			sst.notOk(numberIsNaN(), 'undefined is not NaN');
+			sst.notOk(numberIsNaN(null), 'null is not NaN');
+			sst.notOk(numberIsNaN(false), 'false is not NaN');
+			sst.notOk(numberIsNaN(true), 'true is not NaN');
+			sst.notOk(numberIsNaN(0), 'positive zero is not NaN');
+			sst.notOk(numberIsNaN(Infinity), 'Infinity is not NaN');
+			sst.notOk(numberIsNaN(-Infinity), '-Infinity is not NaN');
+			sst.notOk(numberIsNaN('foo'), 'string is not NaN');
+			sst.notOk(numberIsNaN('NaN'), 'string NaN is not NaN');
+			sst.end();
+		});
+
 		st.notOk(numberIsNaN([]), 'array is not NaN');
 		st.notOk(numberIsNaN({}), 'object is not NaN');
 		st.notOk(numberIsNaN(function () {}), 'function is not NaN');
-		st.notOk(numberIsNaN('NaN'), 'string NaN is not NaN');
+
 		st.test('valueOf', function (vt) {
 			var obj = { valueOf: function () { return NaN; } };
 			vt.ok(numberIsNaN(Number(obj)), 'object with valueOf of NaN, converted to Number, is NaN');
