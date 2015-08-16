b1a52cc882397a5cd40a7ba7ef09907a70349960
Jordan Harband <ljharb@gmail.com>
2015-08-16 02:28:42 -0700

    Implement the [es-shim API](es-shims/api).

--- index.js
+++ index.js
@@ -2,19 +2,16 @@
 
 var define = require('define-properties');
 
+var implementation = require('./implementation');
+var getPolyfill = require('./polyfill');
+var shim = require('./shim');
+
 /* http://www.ecma-international.org/ecma-262/6.0/#sec-number.isnan */
 
-var numberIsNaN = function isNaN(value) {
-	return value !== value;
-};
-
-define(numberIsNaN, {
-	shim: function shimNumberIsNaN() {
-		if (!Number.isNaN) {
-			define(Number, { isNaN: numberIsNaN });
-		}
-		return Number.isNaN || numberIsNaN;
-	}
+define(implementation, {
+	getPolyfill: getPolyfill,
+	implementation: implementation,
+	shim: shim
 });
 
-module.exports = numberIsNaN;
+module.exports = implementation;
